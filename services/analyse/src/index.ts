d5dbcb172d844147b2ce82417c53574eba3c3e91
JoeJGast <gastjosephj@gmail.com>
2021-01-13 20:24:55 -0800

    Added dataDiagram and some psudocode

--- services/analyse/src/index.ts
+++ services/analyse/src/index.ts
@@ -1,3 +1,13 @@
 import RedditRequestHandler from "./requestHandlers/redditRequestHandler/redditRequestHandler"
 
 let requestHandler = new RedditRequestHandler();
+//let databaseAdapter = new postgresDBAdapter()
+
+//let analyzer = new dataAnalyser
+
+// Listen to RSS feed
+    // on new RSS feed item
+        // get comment that mention came from
+        // get parent comment
+        // get user from parent comment
+        // get user's last n comments
